a04773e5beb34152507de1762ee9a6dbbe57d2b6
arjad <arjadgohar14@gmail.com>
2025-04-20 16:11:07 +0500

    added bigger text box

--- public/content.js
+++ public/content.js
@@ -1,12 +1,14 @@
 window.addEventListener("load", () => {
-    
-    const ok = document.querySelector('div.mx-auto.top-5.w-full.z-10');
+  const ok = document.querySelector('div.mx-auto.top-5.w-full.z-10');
+  if (ok) {
     ok.classList.remove("max-w-2xl");
-
-    const targetDiv = document.querySelector('div[aria-label="Write your prompt to Claude"]');
+  }
+  const targetDiv = document.querySelector('div[aria-label="Write your prompt to Claude"]');
+  if (targetDiv) {
     targetDiv.style.height = "250px";
-
-    const okk = document.querySelector('div[aria-label="Write your prompt to Claude"] div');
-    okk.classList.remove("max-w-[60ch]");
-
+  }
+  const okk = document.querySelector('div[aria-label="Write your prompt to Claude"] div');
+  if (okk) {
+      okk.classList.remove("max-w-[60ch]");
+  }
 });
